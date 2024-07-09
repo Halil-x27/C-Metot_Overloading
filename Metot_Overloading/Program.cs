cc100f3f02cf324367d6c1115c8bb8f245eea5bd
Halil Özcan <131258476+Halil-x27@users.noreply.github.com>
2024-07-09 14:38:10 +0300

    Add files via upload

---
+++ Metot_Overloading/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace MetotOverloading
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // out parametreler
+
+            string sayi = "999";
+            int outsayi;
+
+           bool sonuc = int.TryParse(sayi, out outsayi);
+
+           if(sonuc)
+           {
+            Console.WriteLine("Başarılı");
+            Console.WriteLine(outsayi);
+
+           }else{
+            Console.WriteLine("başarısız");
+
+           }
+
+           Metotlar ornek = new Metotlar();
+           ornek.Topla(2,3, out int Toplam);
+
+           Console.WriteLine(Toplam);
+
+
+           // Metot Aşırı Yükleme - Overloading
+
+           int ifade = 999;
+           ornek.EkranaYazidir(ifade);
+        }
+
+    }
+
+    class Metotlar
+    {
+        public  void Topla(int a, int b, out int Toplam)
+        {
+            Toplam = a+b;
+        }
+
+        public void EkranaYazidir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+         public void EkranaYazidir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+    }
+}
